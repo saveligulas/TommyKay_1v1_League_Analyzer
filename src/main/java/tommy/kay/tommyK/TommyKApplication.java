b665620826294665a3502f64b7c13d3b51be87c0
saveligulas <saveli.gulas@gmail.com>
2023-08-02 22:09:14 +0200

    Initial commit

---
+++ src/main/java/tommy/kay/tommyK/TommyKApplication.java
@@ -0,0 +1,13 @@
+package tommy.kay.tommyK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TommyKApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TommyKApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tommy/kay/tommyK/TommyKApplicationTests.java
@@ -0,0 +1,13 @@
+package tommy.kay.tommyK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TommyKApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
