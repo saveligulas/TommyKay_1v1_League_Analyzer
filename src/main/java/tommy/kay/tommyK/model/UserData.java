7f8f32d0fd0cb0e66d3dda8226f097afc1b20b91
saveligulas <saveli.gulas@gmail.com>
2023-08-03 21:22:27 +0200

    added data

--- src/main/java/tommy/kay/tommyK/model/UserData.java
+++ src/main/java/tommy/kay/tommyK/model/UserData.java
@@ -1,7 +1,14 @@
 package tommy.kay.tommyK.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
 @Entity
 public class UserData {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
 }
